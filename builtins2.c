3a2598a3f225abd43d407e897f1217b8eeb790b1
Maggie-Ciku <margaret.wanjiku0883@gmail.com>
2022-08-10 21:25:40 +0300

    simple shell

--- builtins2.c
+++ builtins2.c
@@ -112,5 +112,7 @@ int _atoi(char *str)
 		if ((i == digits - 2) && (str[i + 1] != '\0') && (num > INT_MAX / 10))
 			return (-1);
 	}
-	if (i >  urn (num);
+	if (i > digits)
+		return (-1);
+	return (num);
 }
